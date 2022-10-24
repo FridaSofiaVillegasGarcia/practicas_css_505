4a98c0b92ddb3dc134a90fdf7cb84f9dd1b2e3f8
Maite Rendon <maiterendon2906@gmail.com>
2022-10-24 08:25:25 -0500

    QR Y CLIMA

--- 13/java.js
+++ 13/java.js
@@ -1,32 +1,31 @@
-const VerClima=  ()=>{
-let apikey= "ee1e5278fc35d18f5d761e4b3f3bfa0e";
-let divRes=document.querySelector("#res");
-if(navigator.geolocation){
-    var success = async function(position){
-    var latitud = position.coords.latitude,
-            longitud = position.coords.longitude;
-            console.log("Latitud:  " +latitud +",Longitud: " + longitud);
-let url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${apikey}&units=metric&lang=es `;
-
-        const  api= await fetch(url);
-        const  data= await api.json();
-        console.log(data);
-        const urlIcon = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-        divRes.innerHTML=`
-        <h2> ${data.name.toUpperCase()}</h2>
-        <p> Temperatura : ${data.main.temp} °C </p>
-        <img src="${urlIcon}">  
-        <h2>${data.weather[0].description}</h2>
-        <p> MIN:${data .main.temp_min}°C</p>
-        <p> MAX:${data .main.temp_max}°C</p>
-        <p> PRESION:${data .main.pressure}pda</p>
-        <p> HUMEDAD:${data .main.humidity}</p>
-        <p> VELOCIDAD DE VIENTO:${data.wind.speed} k/hr</p>
-        
-        `;
+const verClima=async()=>{
+        let apiK="ee1e5278fc35d18f5d761e4b3f3bfa0e";
+        let divF=document.querySelector("#res");
+        if(navigator.geolocation){
+            var success = async function(position){
+            var latitud = position.coords.latitude,
+                longitud = position.coords.longitude;
+                console.log("Latitud:"+latitud+", Longitud"+ longitud)
+           let URL=`https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${apiK}&units=metric&lang=es`;
+                console.log(URL);
+    const api= await fetch(URL);
+    const data= await api.json();
+    console.log(data);
+    const urlIcon = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    divF.innerHTML=`
+    <img src="${urlIcon}">
+    <h2>${data.name},${data.sys.country}</h2>
+    <h1>${data.weather[0].description.toUpperCase()}</h1>
+    <h2>Tem.${data.main.temp}</h2>
+    <h1>Tem. Min.${data.main.temp_min}</h1>
+    <h1>Tem. Max${data.main.temp_max}</h1>
+    <h1>Presión At.${data.main.pressure}hPa</h1>
+    <h1>Humedad.${data.main.humidity}%</h1>
+    <h1>V. Viento.${data.wind.speed}Km/hr</h1>
+    `;
         }
-        navigator.geolocation.getCurrentPosition(success, function(msg){
-        console.error( msg );
-        });
-        } 
-}
+            navigator.geolocation.getCurrentPosition(success, function(msg){
+            console.error( msg );
+            });
+            }
+    }
